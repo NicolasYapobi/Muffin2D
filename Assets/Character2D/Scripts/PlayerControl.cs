4dabf2461ff5efb6ce96c5b40c7354c2eae97fe8
Nicolas Yapobi <nicolas.yapobi@epitech.eu>
2021-10-19 07:27:07 +0200

    Initial commit

---
+++ Assets/Character2D/Scripts/PlayerControl.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerControl : MonoBehaviour
+{
+	public bool facingRight = true;
+	public float MovementSpeed = 1;
+	public float JumpForce = 1;
+
+	private Rigidbody2D _rigidBody;
+
+	private void Start() 
+	{
+		_rigidBody = GetComponent<Rigidbody2D>();
+	}
+	
+	
+	private void Update()
+    {
+		var movement = Input.GetAxis("Horizontal");
+		transform.position += new Vector3(movement, 0, 0) * Time.deltaTime * MovementSpeed;
+
+		if (!Mathf.Approximately(0, movement))
+			transform.rotation = movement < 0 ? Quaternion.Euler(0, 200, 0) : Quaternion.identity;
+
+		if (Input.GetButtonDown("Jump") && Mathf.Abs(_rigidBody.velocity.y) < 0.001f)
+        {
+			_rigidBody.AddForce(new Vector2(0, JumpForce), ForceMode2D.Impulse);
+        }
+    }
+}
