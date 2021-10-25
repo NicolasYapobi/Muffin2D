3ff42a871e5d7e8b53f2661325b4d0fffddcc68d
valentin112 <63353873+valentin112@users.noreply.github.com>
2021-10-25 21:09:09 +0900

    Add files via upload

---
+++ Assets/Enemy/script/EnemyMovement.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    private float moveSpeed = 2f;
+    private Rigidbody2D rb;
+    public bool facingRight;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        facingRight = false;
+    }
+
+    private void Update() {
+        if (facingRight) {
+            transform.position += new Vector3(1, 0, 0) * Time.deltaTime * moveSpeed;   
+        } else {
+            transform.position += new Vector3(-1 * Time.deltaTime * moveSpeed, 0, 0);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Wall"))
+        {
+            facingRight = !facingRight;
+            // if (facingRight == true) {
+            //     facingRight = false;
+            // } else {
+            //     facingRight = true;
+            // }
+        }
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            facingRight = !facingRight;
+            // if (facingRight == true) {
+            //     facingRight = false;
+            // } else {
+            //     facingRight = true;
+            // }
+        }
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Destroy(gameObject);
+        }
+        if (collision.gameObject.CompareTag("Fall"))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Enemy/script/Spawner.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System;
+using UnityEngine;
+
+[Serializable]
+public class IntList
+{
+    public int len;
+    public int [] nb;
+}
+
+public class Spawner : MonoBehaviour
+{
+	public GameObject [] Enemy;
+	private float timebtwspawn = 1.5f;
+	private float timebtwspawnGroups;
+	public float startTimeBtwSpawn;
+	public float startTimeBtwSpawnGroups;
+    public Vector3[] spawnPos;
+    public IntList [] enemyType;
+    private int indexPos = 0; // index for the position of the spawn
+    private int currentIndex = -1; // index for the enemy type to spawn next
+    private int currentYIndex = -1; // index for the enemy type to spawn next
+
+    void Update()
+    {
+        if (timebtwspawn <= 0)
+        {
+            if (currentIndex == -1) {
+                indexPos += 1;
+                if (indexPos >= spawnPos.Length) {
+                    indexPos = 0;
+                }
+                currentIndex = UnityEngine.Random.Range(0, enemyType.Length);
+                currentYIndex = 0;
+                timebtwspawnGroups = 0;
+            }
+            if (timebtwspawnGroups <= 0) {
+                var newEnemy = Instantiate(Enemy[enemyType[currentIndex].nb[currentYIndex]], spawnPos[indexPos], Quaternion.identity);
+                var scripts = newEnemy.GetComponent<EnemyMovement>();
+                scripts.facingRight = true;
+                timebtwspawnGroups = startTimeBtwSpawnGroups;
+                currentYIndex += 1;
+            } else {
+                timebtwspawnGroups -= Time.deltaTime;
+            }
+            Debug.Log(currentYIndex);
+            if (currentYIndex >= enemyType[currentIndex].len) {
+                timebtwspawn = startTimeBtwSpawn;
+                currentYIndex = -1;
+                currentIndex = -1;
+            }
+        }
+        else
+        {
+            timebtwspawn -= Time.deltaTime;
+            // Debug.Log("time between spawn :");
+            // Debug.Log(timebtwspawn);
+        }
+	}
+}
